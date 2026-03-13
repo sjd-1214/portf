cc45343fad575b4a19e69cfb3401b8871a8da5bc
sjd-1214 <f230620@cfd.nu.edu.pk>
2026-03-14 01:50:39 +0500

    Github-actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repository name is NOT "sjd-1214.github.io" (e.g., it's "portfolio"), 
+  // you MUST uncomment the line below and set it to your repo name:
+  // basePath: '/portfolio', 
 };
 
 export default nextConfig;
